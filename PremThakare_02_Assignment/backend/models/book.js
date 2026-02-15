da9735c3b2ebab0f2fd71520a13e6dbaaa0efd96
Prem Thakare <142775321+PremThakareitm@users.noreply.github.com>
2026-02-15 13:24:36 +0530

    Delete PremThakare_02_Assignment directory

--- PremThakare_02_Assignment/backend/models/book.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const bookSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  quantity: Number,
-});
-
-module.exports = mongoose.models.Book || mongoose.model('Book', bookSchema);
